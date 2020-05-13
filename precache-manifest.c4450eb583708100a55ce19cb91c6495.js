4981ef2711f287a276bdce293b5ff923cfa8864b
Matthew Bayers <mbayers6370@users.noreply.github.com>
2020-05-12 18:24:10 -0700

    Updates

---
+++ precache-manifest.c4450eb583708100a55ce19cb91c6495.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3930e6c4d576ead6473787102045e1c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41033839c158a3808190",
+    "url": "/static/css/main.7a6091be.chunk.css"
+  },
+  {
+    "revision": "5cf7e194ca573aee4d5c",
+    "url": "/static/js/2.d58c7b72.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d58c7b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41033839c158a3808190",
+    "url": "/static/js/main.856cd860.chunk.js"
+  },
+  {
+    "revision": "ac1d4b092d5fcc4e5485",
+    "url": "/static/js/runtime-main.23868cd1.js"
+  },
+  {
+    "revision": "4ccd5a74ba0221ab05f754b2d4ae7568",
+    "url": "/static/media/Resume.4ccd5a74.pdf"
+  },
+  {
+    "revision": "5ad0c0dfbedfd2f8913714aca62df755",
+    "url": "/static/media/paper.5ad0c0df.jpg"
+  },
+  {
+    "revision": "14d2133db470a378eb5b26d3f195d929",
+    "url": "/static/media/waves.14d2133d.mp4"
+  }
+]);
