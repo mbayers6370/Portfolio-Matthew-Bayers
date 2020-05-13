8925c33aafc06a293cb1870e7a720a7e31d4134c
Matthew Bayers <mbayers6370@users.noreply.github.com>
2020-05-12 18:37:38 -0700

    Updates

---
+++ precache-manifest.ed6e6cd680531eb145ad49686666d99f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e5b9eeda68501c8088d6646be6e045",
+    "url": "/Portfolio-Matthew-Bayers/index.html"
+  },
+  {
+    "revision": "04d11c1ce60449851d41",
+    "url": "/Portfolio-Matthew-Bayers/static/css/main.cfff5cf2.chunk.css"
+  },
+  {
+    "revision": "5cf7e194ca573aee4d5c",
+    "url": "/Portfolio-Matthew-Bayers/static/js/2.d58c7b72.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio-Matthew-Bayers/static/js/2.d58c7b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d11c1ce60449851d41",
+    "url": "/Portfolio-Matthew-Bayers/static/js/main.1dbbaf1d.chunk.js"
+  },
+  {
+    "revision": "17bb98eb731016422410",
+    "url": "/Portfolio-Matthew-Bayers/static/js/runtime-main.29b3ea43.js"
+  },
+  {
+    "revision": "4ccd5a74ba0221ab05f754b2d4ae7568",
+    "url": "/Portfolio-Matthew-Bayers/static/media/Resume.4ccd5a74.pdf"
+  },
+  {
+    "revision": "5ad0c0dfbedfd2f8913714aca62df755",
+    "url": "/Portfolio-Matthew-Bayers/static/media/paper.5ad0c0df.jpg"
+  },
+  {
+    "revision": "14d2133db470a378eb5b26d3f195d929",
+    "url": "/Portfolio-Matthew-Bayers/static/media/waves.14d2133d.mp4"
+  }
+]);
